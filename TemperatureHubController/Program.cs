1997b7251ce9bce6aed73afab3cc10e90ac48294
Łukasz Czepielik <koragrex@gmail.com>
2020-12-30 20:02:58 +0100

    Add project files.

---
+++ TemperatureHubController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemperatureHubController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
