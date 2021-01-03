481495575c9f19feabb8a5b8b2724d7bd84a645b
Łukasz Czepielik <koragrex@gmail.com>
2021-01-03 15:24:54 +0100

    Refactored Core.js component.

--- TemperatureTelemetryWebApp/ClientApp/src/components/Core.js
+++ TemperatureTelemetryWebApp/ClientApp/src/components/Core.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { NotificationManager } from 'react-notifications';
 import { Charts } from './Charts';
-import { MDBBtn } from "mdbreact";
+import { MDBBtn, MDBInput, MDBFormInline, MDBRow, MDBCol, MDBCard, MDBCardBody } from "mdbreact";
+import axios from 'axios';
 
 export class Core extends Component {
     static displayName = Core.name;
@@ -13,19 +15,21 @@ export class Core extends Component {
 
             setTemperature: 0,
             hysteresis: 0,
-            heaterPowerLevel: 0
+            powerLevel: 0
         }
     }
 
     changeValue(e) {
+        console.log(e);
         this.setState({
             [e.target.name]: e.target.value
         });
     }
 
     onClick = nr => () => {
+        console.log(nr);
         this.setState({
-            heaterPowerLevel: nr
+            powerLevel: nr
         });
     };
 
@@ -36,22 +40,23 @@ export class Core extends Component {
     }
 
     componentDidMount() {
-        getHeaterSettings();
+        this.getHeaterSettings();
     }
 
-    getHeaterSettings = async (e) => {
-        e.preventDefault();
-
+    getHeaterSettings = async () => {
         await axios.get("/api/TemperatureTelemetry/GetHeaterSettings", null,
             {
                 params: {
                 }
             }).then(res => {
+                console.log(res);
+                console.log(this.state.powerLevel);
                 this.setState({
-                    setTemperature: res.setTemperature,
-                    hysteresis: res.hysteresis,
-                    powerLevel: res.heaterPowerLevel
+                    setTemperature: res.data.setTemperature,
+                    hysteresis: res.data.hysteresis,
+                    powerLevel: res.data.powerLevel
                 });
+                console.log(this.state.powerLevel);
             }).catch(err => {
             })
     };
@@ -64,7 +69,7 @@ export class Core extends Component {
                 params: {
                     setTemperature: this.state.setTemperature,
                     hysteresis: this.state.hysteresis,
-                    powerLevel: this.state.heaterPowerLevel
+                    powerLevel: this.state.powerLevel
                 }
             }).then(res => {
                 NotificationManager.success('Updated', 'Heater Settings');
@@ -76,56 +81,66 @@ export class Core extends Component {
     render() {
         return (
             <div>
+                <MDBRow center middle>
+                    <MDBCol md="8" className="mt-3">
+                        <MDBCard>
+                            <MDBCardBody>
+                                <form onSubmit={e => this.changeHeaterSettings(e)}>
+                                    <p className="h4 text-center py-4">Temperature Settings</p>
 
-                <div className="grid-x grid-padding-x" style={{ marginTop: 30 }}>
-                    <div className="grid-container fluid callout translucent-form-overlay small-10 medium-8 large-4 cell">
-                        <div className="text-center">
-                            <h2>Settings</h2>
-                        </div>
-                        <form onSubmit={e => this.changeHeaterSettings(e)}>
-                            <MDBBtn>Temperature Set: <input type="text" id="temperatureSet" className="form-control form-control-lg" onChange={e => this.changeValue(e)} value={this.state.setTemperature} /></MDBBtn>
-                            <MDBBtn>Hysteresis<input type="text" id="hysteresis" className="form-control form-control-lg" onChange={e => this.changeValue(e)} value={this.state.hysteresis} /></MDBBtn>
+                                    <MDBRow center middle>
+                                        <MDBBtn gradient="blue" style={{ height: 102, width: 330 }}>Temperature Set<input type="text" id="temperatureSet" className="form-control form-control-lg" name="setTemperature" style={{ "text-align": "center"}} onChange={e => this.changeValue(e)} value={this.state.setTemperature} /></MDBBtn>
+                                        <MDBBtn gradient="blue" style={{ height: 102, width: 330 }}>Hysteresis<input type="text" id="hysteresis" className="form-control form-control-lg" name="hysteresis" style={{ "text-align": "center" }} onChange={e => this.changeValue(e)} value={this.state.hysteresis} /></MDBBtn>
 
-                            <MDBFormInline>
-                                <label>Heater Power Level</label>
-                                <MDBInput
-                                    onClick={this.onClick(1)}
-                                    checked={this.state.heaterPowerLevel === 1 ? true : false}
-                                    label='1'
-                                    type='radio'
-                                    id='heaterPowerLevel1'
-                                    containerClass='mr-5'
-                                />
-                                <MDBInput
-                                    onClick={this.onClick(2)}
-                                    checked={this.state.heaterPowerLevel === 2 ? true : false}
-                                    label='2'
-                                    type='radio'
-                                    id='heaterPowerLevel2'
-                                    containerClass='mr-5'
-                                />
-                                <MDBInput
-                                    onClick={this.onClick(3)}
-                                    checked={this.state.heaterPowerLevel === 3 ? true : false}
-                                    label='3'
-                                    type='radio'
-                                    id='heaterPowerLevel3'
-                                    containerClass='mr-5'
-                                />
-                                <MDBInput
-                                    onClick={this.onClick(4)}
-                                    checked={this.state.heaterPowerLevel === 4 ? true : false}
-                                    label='3'
-                                    type='radio'
-                                    id='heaterPowerLevel4'
-                                    containerClass='mr-5'
-                                />
-                            </MDBFormInline>
+                                        <MDBBtn center middle gradient="blue" className="p-2" style={{ height: 102, width: 330 }}>
+                                            Heater Power Level
+                                <MDBFormInline>
+                                                <MDBInput
+                                                    onClick={this.onClick(1)}
+                                                    checked={this.state.powerLevel === 1 ? true : false}
+                                                    label='1'
+                                                    type='radio'
+                                                    id='heaterPowerLevel1'
+                                                    containerClass='ml-4 mr-5'
+                                                />
+                                                <MDBInput
+                                                    onClick={this.onClick(2)}
+                                                    checked={this.state.powerLevel === 2 ? true : false}
+                                                    label='2'
+                                                    type='radio'
+                                                    id='heaterPowerLevel2'
+                                                    containerClass='mr-5'
+                                                />
+                                                <MDBInput
+                                                    onClick={this.onClick(3)}
+                                                    checked={this.state.powerLevel === 3 ? true : false}
+                                                    label='3'
+                                                    type='radio'
+                                                    id='heaterPowerLevel3'
+                                                    containerClass='mr-5'
+                                                />
+                                                <MDBInput
+                                                    onClick={this.onClick(4)}
+                                                    checked={this.state.powerLevel === 4 ? true : false}
+                                                    label='4'
+                                                    type='radio'
+                                                    id='heaterPowerLevel4'
+                                                    containerClass='mr-4'
+                                                />
+                                            </MDBFormInline>
+                                        </MDBBtn>
+                                    </MDBRow>
 
-                            <MDBBtn outline color="primary" type="submit">Update</MDBBtn>
-                        </form>
-                    </div>
-                </div>
+                                    <div className="text-center py-4 mt-3">
+                                        <MDBBtn outline color="indigo" type="submit">
+                                            Update
+                                         </MDBBtn>
+                                    </div>
+                                </form>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
 
                 <Charts RefreshComponent={this.RefreshComponent} />
 
