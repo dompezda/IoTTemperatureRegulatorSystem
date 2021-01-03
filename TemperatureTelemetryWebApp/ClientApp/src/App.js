18ae629f15f27d77030440e5dd41a293b0c4a5b1
Łukasz Czepielik <koragrex@gmail.com>
2021-01-03 15:24:12 +0100

    Refactored Layout component.

--- TemperatureTelemetryWebApp/ClientApp/src/App.js
+++ TemperatureTelemetryWebApp/ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Authorize } from './components/Authorize';
 import { NotificationContainer } from 'react-notifications';
 
 import './custom.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
--- TemperatureTelemetryWebApp/ClientApp/src/components/Layout.js
+++ TemperatureTelemetryWebApp/ClientApp/src/components/Layout.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { MDBNavbar, MDBNavbarBrand, MDBContainer, MDBFooter} from 'mdbreact';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+    static displayName = Layout.name;
 
-  render () {
-    return (
-        <div>
-            BANER
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+
+                <MDBNavbar color="indigo" dark expand="md">
+                    <MDBNavbarBrand>
+                        <strong className="white-text">Temperature Telemetry Web App</strong>
+                    </MDBNavbarBrand>  
+                </MDBNavbar>
+
+                <MDBContainer fluid className="childContainer">
+                    {this.props.children}
+                </MDBContainer>
+                               
+                <MDBFooter color="indigo" className="font-small pt-4 mt-4 fixed-bottom">
+                    <div className="footer-copyright text-center py-3">
+                        <MDBContainer fluid>
+                            &copy; Copyright {new Date().getFullYear()}: <strong style={{ color: "#fff" }}> Łukasz Czepielik </strong>
+                        </MDBContainer>
+                    </div>
+                </MDBFooter>
+
+            </div>
+        );
+    }
 }
